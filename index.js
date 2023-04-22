804e262218350699b7a671883928eb94930243da
mauro <andrekxus299@gmail.com>
2023-04-21 19:36:12 -0500

    FINAL VERSION 2.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const apiUrl = 'https://swapi.dev/api/people/'
+const url = 'https://swapi.dev/api/people/'
 const cardSection = document.getElementById('card');
 const template = document.getElementById('template');
 
-const people = async () => {
+const people = async (apiUrl) => {
     try {
         const info = await fetch(apiUrl);
         const data = await info.json();
@@ -23,39 +23,47 @@ const people = async () => {
 }
 
 
-const workOnCharacters = async () =>{
-    try{
-    const getCharacters = await people()
-    const fragment = document.createDocumentFragment();
-    getCharacters.slice(0, 10).forEach((character) => {
-        const card = template.content.cloneNode(true);
-        const name = card.getElementById('name');
-        const genre = card.getElementById('genre');
-        const height = card.getElementById('height');
-        const birthYear = card.getElementById('year');
-        const skinColor = card.getElementById('skin');
-        const eyeColor = card.getElementById('eye');
-        const img = card.getElementById('image'); 
-        if(character.gender === 'male'){
-            img.src = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/qui-gon-jinn-1527515078.jpg?crop=1xw:1xh;center,top&resize=480:*';
-        }else if(character.gender === 'female'){
-            img.src = 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/cinefilia/star-wars-mujeres-personajes-femeninos/leia-organa/123062234-1-esl-ES/Leia-Organa.jpg';
-        }else{
-            img.src = 'https://static.vecteezy.com/system/resources/previews/005/217/482/non_2x/the-question-mark-in-a-circle-black-icon-vector.jpg';
-        }
-        name.textContent = character.name;
-        genre.textContent = `Gender: ${character.gender}`;
-        height.textContent = `Height: ${character.height} cm`;
-        birthYear.textContent = `Birth year: ${character.birth_year}`;
-        skinColor.textContent = `Skin color: ${character.skin_color}`;
-        eyeColor.textContent = `Eye color: ${character.eye_color}`;
-       
-        fragment.appendChild(card)
-    });
-    cardSection.appendChild(fragment);
-    }catch (err){
+
+const workOnCharacters = async (newUrl) => {
+    try {
+        const getCharacters = await people(newUrl)
+        const fragment = document.createDocumentFragment();
+        getCharacters.forEach((character) => {
+            const card = template.content.cloneNode(true);
+            const name = card.getElementById('name');
+            const genre = card.getElementById('genre');
+            const height = card.getElementById('height');
+            const birthYear = card.getElementById('year');
+            const skinColor = card.getElementById('skin');
+            const eyeColor = card.getElementById('eye');
+            const img = card.getElementById('image');
+            if (character.gender === 'male') {
+                img.src = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/qui-gon-jinn-1527515078.jpg?crop=1xw:1xh;center,top&resize=480:*';
+            } else if (character.gender === 'female') {
+                img.src = 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/cinefilia/star-wars-mujeres-personajes-femeninos/leia-organa/123062234-1-esl-ES/Leia-Organa.jpg';
+            } else {
+                img.src = 'https://static.vecteezy.com/system/resources/previews/005/217/482/non_2x/the-question-mark-in-a-circle-black-icon-vector.jpg';
+            }
+            name.textContent = character.name;
+            genre.textContent = `Gender: ${character.gender}`;
+            height.textContent = `Height: ${character.height} cm`;
+            birthYear.textContent = `Birth year: ${character.birth_year}`;
+            skinColor.textContent = `Skin color: ${character.skin_color}`;
+            eyeColor.textContent = `Eye color: ${character.eye_color}`;
+
+            fragment.appendChild(card)
+        });
+        cardSection.appendChild(fragment);
+    } catch (err) {
         console.log(err);
     }
 }
 
-workOnCharacters();
+workOnCharacters(url);
+
+const load = document.getElementById("button")
+let page = 2
+load.addEventListener('click', async () => {
+    await workOnCharacters(`https://swapi.dev/api/people/?page=${page}`);
+    page ++;
+});
